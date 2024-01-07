00e23ef6cd20b1d08aa843c61120c7579f677618
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-08 01:20:46 +0400

    Добавили четвертую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -38,16 +38,44 @@
 //     Console.WriteLine("Одна из координат = 0");
 // }
 
-Console.Write("Введите число из отрезка [10,99]: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if (firstDigit > secondDigit)
+// Console.Write("Введите число из отрезка [10,99]: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine(firstDigit);
+// }
+// else
+// {
+//     Console.WriteLine(secondDigit);
+// }
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
 {
-    Console.WriteLine(firstDigit);
+    Console.WriteLine(N);
 }
 else
 {
-    Console.WriteLine(secondDigit);
+    string PrintString = "";
+    string CurrentString = " ";
+    while (N > 0)
+    {
+        int currentDigit = N % 10;
+        N /= 10;
+        CurrentString = currentDigit.ToString();
+        if (N > 0)
+        {
+            PrintString = ("," + CurrentString  + PrintString);
+        }
+        else
+        {
+           
+           PrintString = (CurrentString  + PrintString);
+            Console.WriteLine(PrintString);
+        }
+    }
+   // Console.WriteLine(printString);
 }
-
