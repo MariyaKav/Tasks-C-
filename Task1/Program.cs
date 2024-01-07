805b374db4c8094e551e8cac494a691f115c4927
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-07 21:11:40 +0400

    добавлено решение второй задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,39 @@
-﻿Console.Write("Введите число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-if (firstNumber % 7 == 0 && firstNumber % 23 == 0)
+﻿// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber % 7 == 0 && firstNumber % 23 == 0)
+// {
+//     Console.WriteLine("делится");
+// }
+// else
+// {
+//     Console.WriteLine("не делится");
+// }
+
+Console.Write("Введите координату x, не равную 0: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y, не равную 0: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
 {
-    Console.WriteLine("делится");
+    Console.WriteLine("Первая четверть");
 }
-else
+else if (x < 0 && y > 0)
 {
-    Console.WriteLine("не делится");
+    Console.WriteLine("Вторая четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
+else if (x == 0 && y == 0)
+{
+    Console.WriteLine("Начало координат");
+}
+else 
+{
+    Console.WriteLine("Одна из координат = 0");
 }
