c26808a1b11d186aeee6187faca07db31618dd2c
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-07 21:07:36 +0400

    решение первой задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber % 7 == 0 && firstNumber % 23 == 0)
+{
+    Console.WriteLine("делится");
+}
+else
+{
+    Console.WriteLine("не делится");
+}
