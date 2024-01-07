61c72d3e1477c0f60f80029a7ec667b3b39997ab
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-07 22:05:34 +0400

    добавлено решение третьей задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,31 +9,44 @@
 //     Console.WriteLine("не делится");
 // }
 
-Console.Write("Введите координату x, не равную 0: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату y, не равную 0: ");
-int y = Convert.ToInt32(Console.ReadLine());
-if (x > 0 && y > 0)
+// Console.Write("Введите координату x, не равную 0: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y, не равную 0: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("Первая четверть");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("Вторая четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("Третья четверть");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("Четвертая четверть");
+// }
+// else if (x == 0 && y == 0)
+// {
+//     Console.WriteLine("Начало координат");
+// }
+// else 
+// {
+//     Console.WriteLine("Одна из координат = 0");
+// }
+
+Console.Write("Введите число из отрезка [10,99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
 {
-    Console.WriteLine("Первая четверть");
+    Console.WriteLine(firstDigit);
 }
-else if (x < 0 && y > 0)
+else
 {
-    Console.WriteLine("Вторая четверть");
-}
-else if (x < 0 && y < 0)
-{
-    Console.WriteLine("Третья четверть");
-}
-else if (x > 0 && y < 0)
-{
-    Console.WriteLine("Четвертая четверть");
-}
-else if (x == 0 && y == 0)
-{
-    Console.WriteLine("Начало координат");
-}
-else 
-{
-    Console.WriteLine("Одна из координат = 0");
+    Console.WriteLine(secondDigit);
 }
